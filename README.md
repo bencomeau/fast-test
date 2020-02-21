44f91e0e9bb9473c44914156a5169ae8ca8d829f
Ben Comeau <contact@bencomeau.dev>
2020-02-20 20:19:21 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# fast-test README
+
+Fast Test allows you to quickly run tests on your currently active file.
+
+## Features
+
+- Run a single test on the currently active file.
+- Watch the currently active file for changes and rerun tests related to changed files.
+- Does not run Coverage since the idea is to target development speed while writing tests.
+
+## Requirements
+
+- node: `>=5.2` (support for npx)
+
+## Extension Settings
+
+Coming soon (see **Roadmap** below)
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of Fast Test
+
+-----------------------------------------------------------------------------------------------------------
+
+## Roadmap
+
+### Extension Configuration Support
+
+Add configuration settings to allow:
+
+- setting your test runner (ie: jest, mocha, etc)
+- default terminal (integrated or external)
+
+### External Terminal Support
+
+Allow the extension to execute the test commands to your preferred terminal.
+
+### Tests
+
+Ironically, I need to add tests.
+
+**Enjoy**
---
+++ src/extension.ts
@@ -0,0 +1,72 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+const EXTENSION_NAME: string = 'Fast Test';
+
+/**
+ * Displays the given text in the error message of the window.
+ */
+const error = (text: string): Thenable<string | undefined> => vscode.window.showErrorMessage(text);
+
+/**
+ * Returns the currently active file, if any.
+ */
+const getCurrentFile = (): string | undefined => {
+	const file: string | undefined = vscode.window.activeTextEditor?.document.uri.path;
+
+	if (file) {
+		const workspaceName: string = vscode.workspace.name || "/";
+
+		return file.substring(file.indexOf(workspaceName));
+	}
+
+	return file;
+};
+
+/**
+ * Finds existing Fast Test terminal, if any.
+ */
+const getTerminal = (): vscode.Terminal | undefined => vscode.window.terminals.find(({ name }) => name === EXTENSION_NAME);
+
+export function activate(context: vscode.ExtensionContext) {
+
+	context.subscriptions.push(vscode.commands.registerCommand('extension.test', () => {
+		const file = getCurrentFile();
+
+		if (!file) {
+			return error('You must have an open file in order to run the test!');
+		}
+
+		// Create the terminal, either reusing the existing or creating a new one
+		const terminal = getTerminal() || vscode.window.createTerminal(EXTENSION_NAME);
+
+		// Clear the current terminal's content and show but don't focus
+		terminal.sendText('clear');
+		terminal.show(true);
+
+		// Send the command to the terminal
+		terminal.sendText(`npx jest ${file} --coverage=false`);
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('extension.watch', () => {
+		const file = getCurrentFile();
+
+		if (!file) {
+			return error('You must have an open file in order watch it!');
+		}
+
+		// Create the terminal, either reusing the existing or creating a new one
+		const terminal = getTerminal() || vscode.window.createTerminal(EXTENSION_NAME);
+
+		// Clear the current terminal's content and show but don't focus
+		terminal.sendText('clear');
+		terminal.show(true);
+
+		// Send the command to the terminal
+		terminal.sendText(`npx jest ${file} --watch --coverage=false`);
+	}));
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
